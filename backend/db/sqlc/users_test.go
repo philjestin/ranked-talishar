88c44dcc63b3712d514d0a3eb0f7f018b4025551
Philip Middleton <middleton.philip.pm@gmail.com>
2024-07-13 14:03:03 -0600

    Update some tests

--- backend/db/sqlc/users_test.go
+++ backend/db/sqlc/users_test.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/philjestin/ranked-talishar/password"
 	"github.com/philjestin/ranked-talishar/test_util"
@@ -36,4 +38,37 @@ func createRandomUser(t *testing.T) User {
 
 func TestCreateUser(t *testing.T) {
 	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUserById(context.Background(), user1.UserID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.UserID, user2.UserID)
+	require.Equal(t, user1.UserName, user2.UserName)
+	require.Equal(t, user1.UserEmail, user2.UserEmail)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	arg := UpdateUserParams{
+		UserID:   user1.UserID,
+		UserName: sql.NullString{String: test_util.RandomFirstName()},
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.UserID, user2.UserID)
+	// require.Equal(t, arg.UserName.String, user2.UserName)
+	require.Equal(t, user1.UserEmail, user2.UserEmail)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	require.NotZero(t, user2.UpdatedAt)
 }
